b357ef2a5d4604923c38e4f3a985415493f67aec
giraff <hse05105@naver.com>
2023-08-17 10:16:21 +0900

    settings: pages folder structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from "next/app";
+/** Next.js documents에서 Built-In CSS Support 내용 참조 - 전역 스타일시트는 pages/_app.js 파일에 import */
+import "../styles/reset.css";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
